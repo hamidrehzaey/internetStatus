ed535456ba99f89e21f0ea3b504382604b133141
Hamid Rezaei <hamidrehzaey>
2025-07-28 20:52:29 +0330

    just changed version from 2024.3 to 2025.1.2

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Announces internet status and pressing twice opens the internet systray menu. Shortcut: NVDA+Alt+i"""),
 	# version
-	"addon_version": "2024.3",
+	"addon_version": "2025.1.2",
 	# Author(s)
 	"addon_author": "hamid rezaei",
 	# URL for the add-on documentation support
@@ -37,7 +37,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2019.1",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2024.3",
+	"addon_lastTestedNVDAVersion": "2025.1.2",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
